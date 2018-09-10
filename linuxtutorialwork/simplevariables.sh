b906a6001d26bcd722e72ede4d1a9f825207f4fa
miru <sinogeik@gmail.com>
2018-09-10 21:16:43 +1200

    new thing.

---
+++ linuxtutorialwork/simplevariables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# a simple variable example
+
+myvariable=Hello
+anothervar=Fred
+
+echo $myvariable $anothervar
+echo
+
+sampledir=/home/miru/Git/training-scripts/linuxtutorialwork/foo
+ls /$sampledir
