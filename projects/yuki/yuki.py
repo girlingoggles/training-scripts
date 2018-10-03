018b2cf8648abdeb5dfcaece737cb47611d93825
miru <sinogeik@gmail.com>
2018-10-04 10:29:03 +1300

    I think this shoul dhave all been up to date already?

---
+++ projects/yuki/yuki.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#This is Yuki. She's here to help.
+#Please be patient, she's learning.
+#Yuki is made by girlingoggles
+
+
+def yes_no(yn):
+
+
+def main_menu(opt):
+
+
+def number_loop(num):
+
+def have_chat():
+
+def affirmation():
+
+def cake():
+
+def music():
+
+def leave():
+
+
+
+print("Hello")
+print("My name is Yuki. I'm here to help")
