13c9d8d015ff983fcfbd31359a1f53f2aaa89a24
miru <sinogeik@gmail.com>
2018-10-07 13:20:23 +1300

    started .py

---
+++ projects/speak_number/speak_number.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+#girlingoggles made this
+use strict;
+use warnings;
+
+main(input);
+
+
+
+sub main {
+
+}
+
+sub speak_number_ones {
+    if (one = 1) {
+	print "one ";
+    }    elsif (one = 2) {
+	print "two ";
+    }    elsif (one = 3) {
+	print "three ";
+    }    elsif (one = 4) {
+	print "four ";
+    }    elsif (one = 5) {
+	print "five ";
+    }    elsif (one = 6) {
+	print "six ";
+    }    elsif (one = 7) {
+	print "seven ";
+    }    elsif (one = 8) {
+	print "eight";
+    }    elsif (one = 9) {
+	print "nine ";
+    }
+}
+
+sub speak_number_tens {
+
+}
+
+sub speak_number_teens {
+
+}
+
+sub speak_number_hundreds {
+
+}
+
