628aa105aee2a33c1cfddafd213cffb6eeec541a
girlingoggles <sinogeik@gmail.com>
2018-10-13 18:09:49 +0000

    profile dmp project

--- projects/dmpProfiles/dmpfilestest.py
+++ projects/dmpProfiles/dmpfilestest.py
@@ -1,52 +1,21 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3
 import json
+import Profile
 
-profiles = []
 DumpFile = "yuki.dmp"
 
-#REQUIREMENTS LIST:
-#Global array of profiles(class or dict)
-#Global dmp file name (settable?)
-#Functions:
-#dump/save 
-#load    Assign global profiles, but return profiles or default user?
-#list users
-#reassign default (index 0)
+prof1 = {'name': 'Kyle', 'favorite_color': "Red"}
+prof2 = {'name': 'miru', 'favorite_color': "Blue"}
 
+profile = Profile.Profile(DumpFile, False)
+#profile.add_user(prof1)
+#profile.add_user(prof2)
+#profile.list_users()
+print(profile.get_user("tom"))
 
-def yuki_dump(x):
+#profiles = yuki_load()
 
-    with open(DumpFile, 'w') as outfile:  
-        json.dump(x, outfile)
-
-def yuki_load():
-
-    with open(DumpFile, 'r') as json_file:  
-        data = json.loads(json_file.read())
-    return data
-
-def list_users():
-
-    for user in profiles:
-        print(user['Name'])
-        
-def set_default(user):
-
-    profiles.remove(user)
-    profiles.insert(0, user)
-    
-
-
-prof1 = {'Name': 'Kyle', 'favorite_color': "Blue", 'default': 'yes'}
-prof2 = {'Name': 'miru', 'favorite_color': "Blue", 'default': 'yes'}
-
-#profiles.append(prof1)
-#profiles.append(prof2)
-#yuki_dump(profiles)
-
-profiles = yuki_load()
-
-list_users()
+#list_users()
 
 
 
